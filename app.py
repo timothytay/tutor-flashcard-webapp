8c7822d4334b43295329664e9254ab32acb346b3
Tim Tay <timotheostay@gmail.com>
2023-12-20 12:42:54 +0200

    Set up Flask-SQLAlchemy and user model

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Configures the database file
+app.config['SECRET_KEY'] = 'your_secret_key'  # Needed for session management and security
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
